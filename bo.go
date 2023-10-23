9b4c4ba3127eeed027c1681cdab140f16ade15d1
amarell <mujagicamar@gmail.com>
2023-10-23 22:37:00 +0200

    fell off, touch grass

--- bo.go
+++ bo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 )
 
 func bogo(arr []int) []int {
@@ -36,7 +37,67 @@ func shuffle(arr []int) []int {
 }
 
 func testBogo() {
-	q := []int{39, 3, 5, 8, 13, 21, 34, 99}
+	sortTests := []struct {
+		input    []int
+		expected []int
+		name     string
+	}{
+		//Sorted slice
+		{
+			input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Sorted Unsigned",
+		},
+		//Reversed slice
+		{
+			input:    []int{5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5},
+			name:     "Reversed Unsigned",
+		},
+		//Sorted slice
+		{
+			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Sorted Signed",
+		},
+		//Reversed slice
+		{
+			input:    []int{2, 1, -1, -2, -3},
+			expected: []int{-3, -2, -1, 1, 2},
+			name:     "Reversed Signed ",
+		},
+		//Random order with repetitions
+		{
+			input:    []int{-5, 7, 4, -2, 7},
+			expected: []int{-5, -2, 4, 7, 7},
+			name:     "Random order Signed",
+		},
+		//Single-entry slice
+		{
+			input:    []int{1},
+			expected: []int{1},
+			name:     "Singleton",
+		},
+		// Empty slice
+		{
+			input:    []int{},
+			expected: []int{},
+			name:     "Empty Slice",
+		},
+	}
 
-	fmt.Println(bogo(q))
+	for _, test := range sortTests {
+
+		actual := bogo(test.input)
+		sorted := reflect.DeepEqual(actual, test.expected)
+		if !sorted {
+			fmt.Printf("test %s failed", test.name)
+			fmt.Println()
+			fmt.Printf("actual %v expected %v", actual, test.expected)
+		} else {
+			fmt.Printf("passed %s", test.name)
+			fmt.Println()
+		}
+
+	}
 }
