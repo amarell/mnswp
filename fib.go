6b951f5360f159b75ea663357c9369485f0e1c37
amarell <mujagicamar@gmail.com>
2023-10-23 21:23:52 +0200

    Messing around

---
+++ fib.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	sum := 0
+	a := -1
+	b := 1
+
+	return func() int {
+		sum = 0
+		sum += a + b
+		a = b
+		b = sum
+		return sum
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
