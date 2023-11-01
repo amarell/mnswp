3ce236e2fc8fb1cc707f8b564f2a308201d3eb85
amarell <mujagicamar@gmail.com>
2023-11-01 23:19:44 +0100

    Choose game to play

--- L/main.go
+++ L/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "l/minesweeper"
+import (
+	"fmt"
+	"l/dungeon"
+	"l/minesweeper"
+)
 
 func main() {
-	minesweeper.InitGame()
+	fmt.Println("Choose which game you want to play: ")
+	fmt.Println("1. Dungeon")
+	fmt.Println("2. Minesweeper")
+
+	var choice string
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case "1":
+		dungeon.InitGame()
+	case "2":
+		minesweeper.InitGame()
+	default:
+		fmt.Println("Invalid choice.")
+
+	}
 }
