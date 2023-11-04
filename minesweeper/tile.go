6841f77983f2fb041711cdd39559c1931e1efb3b
amarell <mujagicamar@gmail.com>
2023-11-04 15:12:18 +0100

    Colors in terminal

--- minesweeper/tile.go
+++ minesweeper/tile.go
@@ -2,6 +2,29 @@ package minesweeper
 
 import "fmt"
 
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
+	ColorPurple = "\033[35m"
+	ColorCyan   = "\033[36m"
+	ColorWhite  = "\033[37m"
+)
+
+var colorMap = map[int]string{
+	0: ColorBlue,
+	1: ColorYellow,
+	2: ColorGreen,
+	3: ColorRed,
+	4: ColorCyan,
+	5: ColorRed,
+	6: ColorRed,
+	7: ColorRed,
+	8: ColorRed,
+}
+
 type Tile struct {
 	// x        int
 	// y        int
@@ -19,12 +42,16 @@ func NewTile(isBomb bool) Tile {
 
 func (t Tile) String() string {
 	if t.revealed && t.isBomb {
-		return fmt.Sprint("[*]")
+		return coloredString("[*]", ColorRed)
 	} else if t.flagged {
-		return "[!]"
+		return coloredString("[!]", ColorPurple)
 	} else if t.revealed {
-		return fmt.Sprintf("[%d]", t.val)
+		return coloredString(fmt.Sprintf("[%d]", t.val), colorMap[t.val])
 	} else {
 		return fmt.Sprint("[ ]")
 	}
+}
+
+func coloredString(str, color string) string {
+	return fmt.Sprintf("%s%s%s", color, str, ColorReset)
 }
